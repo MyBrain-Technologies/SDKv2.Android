81da25546de7df4da5da3fb60e08fa0f1f8816fd
mbt-Sophie-Z <sophie.zecri@mybraintech.com>
2019-05-07 15:54:59 +0200

    Corrections to display a pop up when entering the app if permissions are not granted by the user

--- app/src/full/java/mbtsdk/com/mybraintech/sdkv2/HomeActivity.java
+++ app/src/full/java/mbtsdk/com/mybraintech/sdkv2/HomeActivity.java
@@ -1,10 +1,14 @@
 package mbtsdk.com.mybraintech.sdkv2;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -159,6 +163,12 @@ public class HomeActivity extends AppCompatActivity{
         initScanButton();
         initDeviceNamePrefix();
         initDeviceQrCodePrefix();
+        int PERMISSION_ALL = 1;
+
+        String[] PERMISSIONS = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION};
+
+        if(!hasPermissions(getApplicationContext(), PERMISSIONS))
+            ActivityCompat.requestPermissions(HomeActivity.this, PERMISSIONS, PERMISSION_ALL);
     }
 
     private void initConnectAudioSwitch() {
@@ -220,6 +230,16 @@ public class HomeActivity extends AppCompatActivity{
         });
     }
 
+    public static boolean hasPermissions(Context context, String... permissions) {
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && context != null && permissions != null) {
+            for (String permission : permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 
     private void startScan() {
         isErrorRaised = false;
