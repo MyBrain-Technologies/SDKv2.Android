6e2ff8a9e696db85b9846d892165d9478f8966cc
Mehdi Morouche <mehdi.morouche@teedji.com>
2021-02-01 11:33:43 +0100

    update firmware version

--- mbtsdk/build/generated/source/buildConfig/full/debug/mbtsdk/com/mybraintech/mbtsdk/BuildConfig.java
+++ mbtsdk/build/generated/source/buildConfig/full/debug/mbtsdk/com/mybraintech/mbtsdk/BuildConfig.java
@@ -8,13 +8,13 @@ public final class BuildConfig {
   public static final String LIBRARY_PACKAGE_NAME = "mbtsdk.com.mybraintech.mbtsdk";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "full";
-  public static final int VERSION_CODE = 23;
-  public static final String VERSION_NAME = "2.2.12";
+  public static final int VERSION_CODE = 25;
+  public static final String VERSION_NAME = "2.2.14";
   // Fields from default config.
   public static final boolean BLUETOOTH_ENABLED = true;
   public static final boolean DEVICE_ENABLED = true;
   public static final boolean EEG_ENABLED = true;
-  public static final String FIRMWARE_VERSION = "1_7_24";
+  public static final String FIRMWARE_VERSION = "1_7_25";
   public static final boolean RECORDING_ENABLED = true;
   public static final String USE_ALGO_VERSION = "2.3.1";
 }
