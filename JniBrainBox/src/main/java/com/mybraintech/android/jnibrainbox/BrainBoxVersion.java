225f4a0b23cc60d7eac19ba35bd5dffd954141ad
mbt-AnhTuan-M <anhtuan.mai@mybraintech.com>
2021-10-11 14:21:34 +0200

    fix bug kotlin class not found

--- JniBrainBox/src/main/java/com/mybraintech/android/jnibrainbox/BrainBoxVersion.java
+++ JniBrainBox/src/main/java/com/mybraintech/android/jnibrainbox/BrainBoxVersion.java
@@ -1,18 +1,17 @@
-package com.mybraintech.android.jnibrainbox
+package com.mybraintech.android.jnibrainbox;
 
-object BrainBoxVersion {
+public class BrainBoxVersion {
 
     /**
      * the version number must be validated by research team and data team
      */
-    private const val BRAIN_BOX_VERSION = "3.0.0"
+    private static String BRAIN_BOX_VERSION = "3.0.0";
 
     /**
      * ATTENTION: 10 July 2021: in the file Version.hpp, the version is noted as 1.0.0 but we decided to put the value 3.0.0 since the previous version was 2.14.0
-     * @see Version.hpp
+     * @see BrainBox/Version.hpp
      */
-    @JvmStatic
-    fun getVersion(): String {
-        return BRAIN_BOX_VERSION
+    public static String getVersion() {
+        return BRAIN_BOX_VERSION;
     }
 }
