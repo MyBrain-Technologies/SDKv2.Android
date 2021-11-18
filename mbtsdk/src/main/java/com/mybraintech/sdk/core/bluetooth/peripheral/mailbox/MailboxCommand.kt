ba77ec90d21c0f3321c275233de8d7be8131b380
Laurent Noudohounsi <laurent.noudohounsi@mybraintech.com>
2021-11-18 01:18:58 +0100

    Add MailboxCommand

---
+++ mbtsdk/src/main/java/com/mybraintech/sdk/core/bluetooth/peripheral/mailbox/MailboxCommand.kt
@@ -0,0 +1,107 @@
+package com.mybraintech.sdk.core.bluetooth.peripheral.mailbox
+
+// TODO: Or use sealed class?
+enum class MailboxCommand(val raw: Byte) {
+  // Product name configuration request
+  SetProductName(0x02.toByte()), // Only melomind
+
+  /// Used by app to request an OTA update (provides software major and minor
+  ///  in payload)
+  StartOTATFX(0x03.toByte()),
+
+  /// Notifies app of a lead off modification
+//  leadOffEvent(0x04 // Only melomind. Not used. For reference.
+
+  /// Notifies appli that we switched to OTA mode
+  OtaModeEvent(0x05.toByte()),
+
+  /// Notifies appli that we request a packet Idx reset
+  OtaIndexResetEvent(0x06.toByte()),
+
+  /// Notifies appli with the status of the OTA transfert.
+  OtaStatusEvent(0x07.toByte()),
+
+  /// allows to retrieve to system global status
+  SystemGetStatus(0x08.toByte()),
+
+  /// trigger a reboot event at disconnection
+  SystemRebootEvent(0x09.toByte()), // 0x09, 0x29, 0x08
+
+  /// Set the melomind serial nb
+  SetSerialNumber(0x0A.toByte()),
+
+  SetA2dpName(0x1A.toByte()), // Set QRCode
+
+  /// allows to hotswap the filters' parameters
+  SetNotchFilter(0x0B.toByte()),
+
+  /// Set the signal bandwidth by changing the embedded bandpass filter
+  SetBandpassFilter(0x0C.toByte()),
+
+  /// Set the eeg signal amplifier gain
+  SetAmplifierSignalGain(0x0D.toByte()),
+
+  /// Get the current configuration of the Notch filter, the bandpass filter,
+  /// and the amplifier gain.
+  GetEEGConfig(0x0E.toByte()),
+
+  /// Enable or disable the p300 functionnality of the melomind.
+  ToggleP300(0x0F.toByte()),
+
+  EnableDCOffset(0x10.toByte()),
+
+  A2dpConnection(0x11.toByte()),
+
+  /////////////
+  BatteryLevel(0x20.toByte()),
+
+  SerialNumber(0x22.toByte()),
+
+  DeviceId(0x23.toByte()),
+
+  /// Response: 0 failure, 1 success
+  StartEeg(0x24.toByte()),
+
+  // Response: 0 failure, 1 success
+  StopEeg(0x25.toByte()),
+
+  Reset(0x26.toByte()),
+
+  FirmewareVersion(0x27.toByte()),
+
+  HardwareVersion(0x28.toByte()),
+
+  MtuSize(0x29.toByte()),
+
+  GetFilterConfigurationType(0x30.toByte()),
+
+  SetFilterConfigurationType(0x31.toByte()),
+
+  SetAudioconfig(0x32.toByte()),
+
+  StartImsAcquisition(0x33.toByte()),
+
+  StopImsAcquisition(0x34.toByte()),
+
+  StartPpgAcquisition(0x35.toByte()),
+
+  StopPpgAcquisition(0x36.toByte()),
+
+  StartTemperatureAcquisition(0x37.toByte()),
+
+  StopTemperatureAcquisition(0x38.toByte()),
+
+  SetImsConfiguration(0x39.toByte()),
+
+  GetSensorStatus(0x41.toByte()),
+
+  SetPpgConfiguration(0x42.toByte()),
+
+  ImsDataFrameEvent(0x50.toByte()),
+
+  PpgDataFrameEvent(0x60.toByte()),
+
+  EegDataFrameEvent(0x40.toByte()),
+
+  UnknownEvent(0xFF.toByte())
+}
