b20326f35a6ffc1da3e95fa88bd667bd3f11b719
Laurent Noudohounsi <laurent.noudohounsi@mybraintech.com>
2021-11-18 09:49:22 +0100

    Add IndusVersion and HardwareVersion

---
+++ mbtsdk/src/main/java/com/mybraintech/sdk/core/bluetooth/deviceinformation/HardwareVersion.kt
@@ -0,0 +1,8 @@
+package com.mybraintech.sdk.core.bluetooth.deviceinformation
+
+enum class HardwareVersion(val raw: String) {
+  v_1_0_0("1.0.0"),
+  v_1_1_0("1.1.0"),
+  v_2_0_0("2.0.0"),
+  v_2_1_0("2.1.0")
+}
