dcdac2b68ab60f96b59a5fc657aa571828a932c8
mbt-anhtuan-m <anhtuan.mai@mybraintech.com>
2023-05-31 15:26:03 +0200

    SDK-450 : allow 11 chars serial number for Indus5

--- sdk-v3/src/main/java/com/mybraintech/sdk/core/bluetooth/devices/qplus/Indus5DeviceImpl.kt
+++ sdk-v3/src/main/java/com/mybraintech/sdk/core/bluetooth/devices/qplus/Indus5DeviceImpl.kt
@@ -185,8 +185,8 @@ abstract class Indus5DeviceImpl(ctx: Context) :
 
     override fun setSerialNumber(serialNumber: String, listener: SerialNumberChangedListener?) {
         if (isConnectedAndReady()) {
-            if (serialNumber.length != 10) {
-                listener?.onSerialNumberError("Serial number length must be 10")
+            if (serialNumber.length != 10 && serialNumber.length != 11) {
+                listener?.onSerialNumberError("Serial number length must be 10 or 11")
                 return
             }
             this.serialNumberChangedListener = listener
