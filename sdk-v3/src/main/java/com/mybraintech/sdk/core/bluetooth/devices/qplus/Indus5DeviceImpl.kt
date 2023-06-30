7279ac53e1d8cbaffad0e096c6f2c1e4bb24efd3
mbt-anhtuan-m <anhtuan.mai@mybraintech.com>
2023-06-30 14:45:05 +0200

    allow 0 to 11 characters audio name

--- sdk-v3/src/main/java/com/mybraintech/sdk/core/bluetooth/devices/qplus/Indus5DeviceImpl.kt
+++ sdk-v3/src/main/java/com/mybraintech/sdk/core/bluetooth/devices/qplus/Indus5DeviceImpl.kt
@@ -202,7 +202,7 @@ abstract class Indus5DeviceImpl(ctx: Context) :
 
     override fun setAudioName(audioName: String, listener: AudioNameListener?) {
         if (isConnectedAndReady()) {
-            if (audioName.length in 6..10) {
+            if (audioName.length <= 11) {
                 this.audioNameListener = listener
                 getSetAudioNameRequest(audioName)
                     .fail { _, failReason ->
